634e81c02e5b485d62cf68cf9efec23af28ff92a
Comebackkid33 <Kurganovk@gmail.com>
2017-07-26 11:29:25 +0300

    пытаемся сделать все хорошо

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
     var fs = require('fs');
-    var Cities = city;
+    var Cities = JSON.parse(fs.readFileSync('./public/JSON/Cities.JSON', 'utf8'));
+    // var Cities = city;
     res.render('index',{title:"ККУ",Cities: Cities});
 });
 
